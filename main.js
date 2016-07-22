9841220ebd6a70c5813ab91f97b7fa582a71f9ad
HectorHurtado <hector-fha@hotmail.com>
2016-07-21 19:39:24 -0500

    Correccion dibujar bola con  beginPath

--- main.js
+++ main.js
@@ -112,6 +112,7 @@
 				ctx.fillRect(element.x,element.y,element.width,element.height);
 				break;
 			case "circle":
+				ctx.beginPath();
 				ctx.arc(element.x,element.y,element.radius,0,7);
 				ctx.fill();
 				ctx.closePath();
